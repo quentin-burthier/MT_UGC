2d28f663063cd12793a09aae103050ee489c8687
Quentin Burthier <quentin.burthier@ensta-paris.fr>
2020-09-23 16:12:43 +0200

    removed wmt17-transformer

--- wmt2017-transformer/scripts/validate.en.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cat $1 \
-    | sed 's/\@\@ //g' \
-    | $TOOLS/moses-scripts/scripts/recaser/detruecase.perl 2>/dev/null \
-    | $TOOLS/moses-scripts/scripts/tokenizer/detokenizer.perl -l en 2>/dev/null \
-    | $TOOLS/moses-scripts/scripts/generic/multi-bleu-detok.perl $DATA/valid.en \
-    | sed -r 's/BLEU = ([0-9.]+),.*/\1/'
