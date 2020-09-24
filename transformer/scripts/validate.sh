7413ea7f3a43edbcc0566a63d25c18273583658b
Quentin Burthier <quentin.burthier@ensta-paris.fr>
2020-09-24 16:01:58 +0200

    feat: marian_train config yaml file

--- transformer/scripts/validate.sh
+++ transformer/scripts/validate.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # suffix of source language files
-src=en
+src=$1
 # suffix of target language files
-tgt=fr
+tgt=$2
 # MTNT and processed data paths
 mtnt=$DATA/MTNT
 dir=$mtnt/$src.$tgt
 
-cat $1 \
+cat $3 \
     | sed 's/\@\@ //g' \
     | $TOOLS/moses-scripts/scripts/recaser/detruecase.perl 2>/dev/null \
     | $TOOLS/moses-scripts/scripts/tokenizer/detokenizer.perl -l $tgt 2>/dev/null \
-    | $TOOLS/moses-scripts/scripts/generic/multi-bleu-detok.perl $DATA/valid.$tgt \
+    | $TOOLS/moses-scripts/scripts/generic/multi-bleu-detok.perl $dir/splitted/valid.$tgt \
     | sed -r 's/BLEU = ([0-9.]+),.*/\1/'
