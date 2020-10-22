f01028205f74db6e046e1bb493672d7ba01a310e
Quentin Burthier <quentin.burthier@inria.fr>
2020-10-22 18:10:31 +0200

    feat(training): back-translation

--- tools/parse_cli.sh
+++ tools/parse_cli.sh
@@ -64,6 +64,17 @@ while (( "$#" )); do
         exit 1
     fi
     ;;
+    -btm|--back-translation-model)
+    if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        bt_model=$2
+        back_translate=true
+        bt="bt."  # hack for input file naming
+        shift 2
+    else
+        back_translate=false
+        shift 1
+    fi
+    ;;
     -o|--output-dir)
     if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
         output_dir=$2
@@ -101,4 +112,41 @@ while (( "$#" )); do
     ;;
   esac
 done
+}
+
+function set_dataset_args() {
+case $dataset in
+    MTNT)
+        if $shuffle; then
+            mtnt=$DATA/MTNT_reshuffled
+        else
+            mtnt=$DATA/MTNT
+        fi
+        dir=$mtnt/$src-$tgt.$ratio
+        mono_dir=$mtnt/monolingual
+        preprocess_args="$src $tgt $dir $mtnt $ratio"
+    ;;
+    europarl_nc)
+        dir=$DATA/europarl_nc.$src-$tgt
+        preprocess_args="$src $tgt $dir"
+    ;;
+    nce_small)
+        dir=$DATA/nce_small.$src-$tgt
+        dataset=europarl_nc
+        preprocess_args="$src $tgt $dir"
+    ;;
+    OpenSubtitles)
+        dir=$DATA/OpenSubtitles.en-fr
+        preprocess_args="$src $tgt $dir"
+    ;;
+    OpenSubtitles_small)
+        dir=$DATA/OpenSubtitles_small.$src-$tgt
+        dataset=OpenSubtitles
+        preprocess_args="$src $tgt $dir"
+    ;;
+esac
+formated_date=$(date +"%d.%m.%Y_%T")
+if [ ! "$output_dir" ]; then output_dir=$dir/output_$formated_date; fi
+if [ ! "$val_output_dir" ]; then val_output_dir=$dir/val_output_$formated_date; fi
+if [ ! "$bt_dir" ]; then bt_dir=$dir/bt_$formated_date; fi
 }
--- transformer/run_experiment.sh
+++ transformer/run_experiment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source $TOOLS/parse_cli.sh
+source $TOOLS/parse_cli.sh  # parse_cli, set_dataset_args
 
 # Default CLI args
 src=en
@@ -8,55 +8,54 @@ tgt=fr
 voc_sz=32000
 
 model_dir=model
+back_translate=false
 
 parse_cli $@
 
 marian_train=$MARIAN/marian
 marian_decoder=$MARIAN/marian-decoder
 
-case $dataset in
-    MTNT)
-        if $shuffle; then
-            mtnt=$DATA/MTNT_reshuffled
-        else
-            mtnt=$DATA/MTNT
-        fi
-        dir=$mtnt/$src-$tgt.$ratio
-        preprocess_args="$src $tgt $dir $mtnt $ratio"
-    ;;
-    europarl_nc)
-        dir=$DATA/europarl_nc.$src-$tgt
-        dataset_args=$tgt
-        preprocess_args="$src $tgt $dir"
-    ;;
-    nce_small)
-        dir=$DATA/nce_small.$src-$tgt
-        dataset_args=$tgt
-        dataset=europarl_nc
-        preprocess_args="$src $tgt $dir"
-    ;;
-    OpenSubtitles)
-        dir=$DATA/OpenSubtitles.$src-$tgt
-        dataset_args=$tgt
-        dataset=europarl_nc
-        preprocess_args="$src $tgt $dir"
-    ;;
-esac
-formated_date=$(date +"%d.%m.%Y_%T")
-if [ ! "$output_dir" ]; then output_dir=$dir/output_$formated_date; fi
-if [ ! "$val_output_dir" ]; then val_output_dir=$dir/val_output_$formated_date; fi
+set_dataset_args
 
 input_dir=$dir/preprocessed
 if [ ! -e "$input_dir" ]
 then
     ./scripts/preprocess_$dataset.sh $preprocess_args
-    echo "Preprocessing done"
-    echo ""
 fi
 
 echo ": Up. 0 :"
 echo "n_lines: $(wc -l $dir/raw/train.$src | cut -d" " -f1)"
 # python $TOOLS/compare_lexicons.py $dir/raw/{train,dev}.$src
+
+# Back-translation: tgt -> src model is assumed to have been trained
+if [ ! -e "$input_dir/train.bt.$src" ]
+then
+    if [ ! -e "$mono_dir/preprocessed/train.$tgt" ]
+    then
+        ./scripts/preprocess_${dataset}_monolingual.sh $tgt $mono_dir
+    fi
+
+    if [ ! -e "$bt_dir/train.$src" ]
+    then
+        mkdir -p $bt_dir
+        $marian_decoder \
+            -i $mono_dir/preprocessed/train.$tgt \
+            -c $bt_model/model.npz.best-translation.npz.decoder.yml \
+            -d $gpus -b 1 -n -w 10000 \
+            --quiet-translation --quiet \
+            --max-length 100 --max-length-crop \
+            > $bt_dir/train.$src
+    fi
+
+    # awk '{print "<syn>" $0 }' $bt_dir/train.$src > $bt_dir/train.$src
+    # awk '{print "<nat>" $0 }' $input_dir.$src > $input_dir.$src
+
+    cat $bt_dir/train.$src $input_dir/train.$src \
+    > $input_dir/train.bt.$src
+
+    cat $mono_dir/preprocessed/train.$tgt $input_dir/train.$tgt \
+    > $input_dir/train.bt.$tgt
+fi
 
 # train model
 if [ ! -e "$model_dir/model.npz" ]
@@ -67,7 +66,7 @@ then
     $marian_train \
         -c config.yml \
         -m $model_dir/model.npz \
-        --train-sets $input_dir/train.{$src,$tgt} \
+        --train-sets $input_dir/train.$bt{$src,$tgt} \
         --valid-sets $input_dir/val.{$src,$tgt} \
         --valid-translation-output "$val_output_dir/epoch.{E}.$tgt" \
         --valid-script-args $tgt $dir/raw/val.$tgt \
@@ -78,26 +77,19 @@ fi
 
 # translate dev sets
 mkdir -p $output_dir
-for split in val dev
+for split in dev
 do
-    cat $input_dir/$split.$src \
-        | $marian_decoder \
-            -c $model_dir/model.npz.decoder.yml \
-            -m $model_dir/model.npz.best-translation.npz \
-            -d $gpus -b 12 -n -w 6000 \
-            --quiet-translation --quiet \
-        | sed 's/\@\@ //g' \
-        | $MOSES_SCRIPTS/recaser/detruecase.perl \
-        | $MOSES_SCRIPTS/tokenizer/detokenizer.perl -l $tgt \
-        > $output_dir/$split.$tgt
+    $marian_decoder \
+        -i $input_dir/$split.$src \
+        -c $model_dir/model.npz.decoder.yml \
+        -d $gpus -b 12 -n -w 6000 \
+        --quiet-translation --quiet \
+    | sed 's/\@\@ //g' \
+    | $MOSES_SCRIPTS/recaser/detruecase.perl \
+    | $MOSES_SCRIPTS/tokenizer/detokenizer.perl -l $tgt \
+    > $output_dir/$split.$tgt
 done
 
 # calculate bleu scores on dev set
 echo ": Up. 0 :"
 cat $output_dir/dev.$tgt | sacrebleu $dir/raw/dev.$tgt
-
-lang=en
-train=europarl_nc.en-fr 
-# dev=$train
-dev=MTNT_reshuffled/en-fr.1.0
-python $TOOLS/compare_lexicons.py $DATA/$train/preprocessed/train.$lang $DATA/$dev/preprocessed/dev.$lang
