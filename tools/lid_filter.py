5f6891fd385de85bb60dae1148805393f2801d6a
Quentin Burthier <quentin.burthier@inria.fr>
2020-12-29 22:39:56 +0100

    fix(lid_filter): correctly writes line by line

--- tools/lid_filter.py
+++ tools/lid_filter.py
@@ -7,8 +7,8 @@ def main(src_path: str, tgt_path: str,
     with open(src_path, "r") as src_raw, open(src_lid_path) as src_lid, open(tgt_path, "r") as tgt_raw, open(tgt_lid_path) as tgt_lid, open(src_filtered, "w") as src_f, open(tgt_filtered, "w") as tgt_f:
         for src_line, src_line_lang, tgt_line, tgt_line_lang in zip(src_raw, src_lid, tgt_raw, tgt_lid):
             if src_line_lang[-3:-1] == src_lang and tgt_line_lang[-3:-1] == tgt_lang:
-                src_f.write(f"{src_line}\n")
-                tgt_f.write(f"{tgt_line}\n")
+                src_f.write(src_line)
+                tgt_f.write(tgt_line)
 
 
 if __name__ == "__main__":
