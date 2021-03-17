b26ff3e500194db7ceec501b624fb2eab8d05327
Quentin Burthier <quentin.burthier@inria.fr>
2021-03-17 12:27:21 +0100

    fix: oar iter args helper

--- tools/val_iter_list.py
+++ tools/val_iter_list.py
@@ -8,54 +8,59 @@ def main_from_cli():
     model_checkpoints_list(
         train_set=args.train_set, dev_set=args.dev_set,
         src=args.src, tgt=args.tgt,
-        arch=args.arch, tokenlevel=args.tokenlevel, voc_sz=args.voc_sz,
+        arch=args.arch, segmentations=args.segmentations,
         framework=args.framework
     )
 
 def main_from_global():
     train_sets = [
-        "News-Commentary",
+        # "News-Commentary",
         "Europarl",
-        "Europarl_small",
-        "OpenSubtitles",
-        "OpenSubtitles_small"
+        # "Europarl_small",
+        # "OpenSubtitles",
+        # "OpenSubtitles_small"
     ]
     dev_sets = [
-        "News-Commentary",
-        "Europarl",
-        "OpenSubtitles",
-        "MTNT",
+        # "News-Commentary",
+        # "Europarl",
+        # "OpenSubtitles",
+        # "MTNT",
         "Crapbank",
-        "Foursquare",
+        # "Foursquare",
     ]
-    tokenlevels_arch = [
-        ("bpe", ""),
-        ("char", ""),
-        ("char", "conv")
+    segmentations_vsz_arch = [
+        ("bpe.32000", 32000, ""),
+        ("char", 128, ""),
+        ("char", 128, "conv")
     ]
-    for train_set, dev_set, (tokenlevel, arch) in itertools.product(train_sets, dev_sets, tokenlevels_arch):
-        args = parse_cli()
+    args = parse_cli()
+    for train_set, dev_set, (segmentations, vsz, arch) in itertools.product(train_sets,
+                                                                            dev_sets,
+                                                                            segmentations_vsz_arch):
         model_checkpoints_list(
             train_set=train_set, dev_set=dev_set,
             src=args.src, tgt=args.tgt,
-            arch=arch, tokenlevel=args.tokenlevel, voc_sz=args.voc_sz,
+            arch=arch, segmentations=segmentations, voc_sz=vsz,
             framework=args.framework
         )
 
 def model_checkpoints_list(train_set: str, dev_set: str,
                            src: str, tgt: str,
-                           arch: str, tokenlevel: str, voc_sz: str,
+                           arch: str,
+                           voc_sz: int,
+                           segmentations: str,
                            framework: str):
     model_dir = os.path.join(
         os.environ["DATA"],
         f"models.{framework}",
-        f"{arch}Trb.{train_set}"
-        f".{tokenlevel if tokenlevel == 'char' else f'tokenlevel.{voc_sz}'}"
+        f"{arch}Trb.{train_set}.{segmentations}"
         f".{src}-{tgt}"
     )
-    for epoch, train_iter in checkpoint_list(model_dir):
-        print(" ".join([framework, src, tgt, train_set, dev_set, tokenlevel, epoch, train_iter, voc_sz, arch]).strip())
-
+    for epoch, train_iter in checkpoint_list(model_dir)[::2]:
+        print(" ".join([train_set, dev_set,
+                        segmentations.split(".")[0],
+                        str(voc_sz),
+                        epoch, train_iter, arch]).strip())
 
 
 def checkpoint_list(model_dir: str) -> list:
@@ -72,8 +77,7 @@ def parse_cli():
     parser.add_argument("--src", type=str, default="fr")
     parser.add_argument("--tgt", type=str, default="en")
     parser.add_argument("--arch", type=str, default="")
-    parser.add_argument("--tokenlevel", type=str, default="char")
-    parser.add_argument("--voc_sz", type=str, default="32000")
+    parser.add_argument("--segmentations", type=str, default="char")
     parser.add_argument("--framework", type=str, default="fairseq")
     return parser.parse_args()
 
